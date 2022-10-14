67e2811e5b564b1ebb5b916641908c16c81cecb5
Badrudeen2 <90875260+Badrudeen2@users.noreply.github.com>
2022-10-13 17:02:16 -0700

    Add files via upload

---
+++ form/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form get</title>
+</head>
+<body>
+    <!--Simple form to get user name and email and submit to user_data.php-->
+    <form method="POST" action="user_data.php">
+        <label for="name">
+            Name: <input type="text" name="name" placeholder="--your name--">
+        </label> <br>
+        <label for="email">
+            E-mail: <input type="email" name="email">
+        </label> <br>
+        <label for="dob">
+            Date-of-birth: <input type="date" name="dob" max="2012-12-12">
+        </label><br>
+        Gender:
+		<label>
+		  	<input type="radio" name="gender" value="male"/>Male
+		</label>
+		<label>
+		  	<input type="radio" name="gender" value="female"/>Female
+		</label><br>
+        <label for="country">
+            Country: <input type="text" name="country" placeholder="country">
+        </label><br>
+		
+      
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
---
+++ form/user_data.php
@@ -0,0 +1,31 @@
+
+<?php
+//extract the content of form posted using their keys
+//$result = print_r($_POST);
+$name = $_POST['name'];
+$email = $_POST['email'];
+$dob = $_POST['dob'];
+$gender = $_POST['gender'];
+$country = $_POST['country'];
+
+
+// print the extracted values out to the user
+echo "NAME: " . $name . "<br>";
+echo "EMAIL: " . $email . "<br>";
+echo "DATEOFBIRTH: " . $dob . "<br>";
+echo "GENDER: " . $gender . "<br>";
+echo "COUNTRY " . $country . "<br>";
+
+//write the output to a csv file
+$filename = "userdata.csv";    
+$handle = fopen($filename, "a");//open file in read mode 
+$no_row = count(file("userdata.csv"));
+//converting array into string
+$result = $no_row.", ".$name.", ".$email.", ".$dob.", ".$gender.", ".$country;
+fwrite($handle,$result."\n");//write to file)  
+fclose($handle);//close file
+print_r($result);
+
+
+
+?>
